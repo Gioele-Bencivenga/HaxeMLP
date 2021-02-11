92cafb8253d12a01e41e351624d451f06cf254d8
Gioele <gioelebenci@live.com>
2021-02-11 23:06:17 +0000

    proper display of layers and weights

--- source/brains/Perceptron.hx
+++ source/brains/Perceptron.hx
@@ -4,7 +4,14 @@ import utilities.HxFuncs;
 import flixel.FlxG;
 
 /**
- * Single Layer Perceptron.
+ * MultiLayer Perceptron.
+ * 
+ * next steps:
+ * do weighted sum for each neuron then pass through activation function
+ * (more computationally efficient if you do sum+tanh for each single neuron instead of doing all the sums then passing all of the sums throught tanh at once) 
+ * 
+ * for activation function:
+ * tanh (hyperbolic function), found in standard Math library
  */
 class Perceptron {
 	/**
@@ -13,6 +20,30 @@ class Perceptron {
 	 * Specify the number of neurons when creating a new instance, each neuron's values is randomly assigned.
 	 */
 	public var inputLayer(default, null):Array<Float>;
+
+	/**
+	 * The perceptron's hidden layer.
+	 */
+	public var hiddenLayer(default, null):Array<Float>;
+
+	/**
+	 * The perceptron's hidden layer.
+	 */
+	public var outputLayer(default, null):Array<Float>;
+
+	/**
+	 * Array of weights for the connections between neurons.
+	 */
+	public var weights(default, null):Array<Float>;
+
+	/**
+	 * Total number of connection weights.
+	 *
+	 * Calculated by doing the sum between:
+	 * - number of weights between input and hidden layers: `input neurons * hidden neurons`
+	 * - number of weights between hidden and output layers: `hidden neurons * output neurons`
+	 */
+	var weightsCount:Int;
 
 	/**
 	 * The rate at which the `Perceptron` learns.
@@ -24,11 +55,17 @@ class Perceptron {
 	 * @param _inputLayerSize the number of neurons that the `inputLayer` will have
 	 * @param learningRate `0.01` by default, it's the `Perceptron`'s learning rate
 	 */
-	public function new(_inputLayerSize:Int, _learningRate = 0.01) {
+	public function new(_inputLayerSize:Int, _hiddenLayerSize:Int, _learningRate = 0.01) {
 		learningRate = _learningRate;
 
-		// each weight is given a random value between -1 and 1
+		// initialise layers with neurons having random values
 		inputLayer = [for (i in 0..._inputLayerSize) FlxG.random.float(-1, 1)];
+		hiddenLayer = [for (i in 0..._hiddenLayerSize) FlxG.random.float(-1, 1)];
+		outputLayer = [for (i in 0...1) FlxG.random.float(-1, 1)];
+
+		// calculate number of connection weights between neurons and initialise them with random values
+		weightsCount = (inputLayer.length * hiddenLayer.length) + (hiddenLayer.length * outputLayer.length);
+		weights = [for (i in 0...weightsCount) FlxG.random.float(-1, 1)];
 	}
 
 	/**
@@ -36,22 +73,20 @@ class Perceptron {
 	 * @param _inputs the array of input inputLayer the Perceptron receives
 	 * @return the summed vector
 	 */
-	//public function feedForward(_input:Vector2):Vector2 {
+	// public function feedForward(_input:Vector2):Vector2 {
 	//	var sum = new Vector2(0, 0);
-
 	//	for (i in 0...inputLayer.length) {
 	//		_input.multiplyWith(inputLayer[i]);
 	//		sum.addWith(_input);
 	//	}
 	//	return sum;
-	//}
-
+	// }
 	/*
-	public function train(_input:Vector2, _error:Vector2) {
-		// Adjust all the inputLayer according to the error and learning rate
-		inputLayer[0] += learningRate * _error.x * _input.x;
-		inputLayer[0] += learningRate * _error.y * _input.y;
-		inputLayer[0] = HxFuncs.constrain(inputLayer[0], 0, 1);
-	}
-	*/
+		public function train(_input:Vector2, _error:Vector2) {
+			// Adjust all the inputLayer according to the error and learning rate
+			inputLayer[0] += learningRate * _error.x * _input.x;
+			inputLayer[0] += learningRate * _error.y * _input.y;
+			inputLayer[0] = HxFuncs.constrain(inputLayer[0], 0, 1);
+		}
+	 */
 }
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -44,14 +44,14 @@ class PlayState extends FlxState {
 	var perc:Perceptron;
 
 	/**
-	 * List displaying the input layer neuron weights.
+	 * Array of our lists displaying layers.
 	 */
-	var inputLayerList:ListView;
+	var layerLists:Array<ListView>;
 
 	/**
-	 * List displaying the output layer neuron weights.
+	 * Array of our lists displaying weights.
 	 */
-	var outputLayerList:ListView;
+	var weightsLists:Array<ListView>;
 
 	override function create() {
 		/// UI STUFF
@@ -64,24 +64,54 @@ class PlayState extends FlxState {
 		uiView.cameras = [uiCam]; // all of the ui components contained in uiView will be rendered by uiCam
 		uiView.scrollFactor.set(0, 0); // and they won't scroll
 		add(uiView);
-		// get the generated component and assign it to the function (xml events are for slower scripting with hscript)
+		// get the generated component and assign it to our function (xml events are for slower scripting with hscript)
 		uiView.findComponent("btn_init_perceptron", MenuItem).onClick = btn_initPerceptron_onClick;
 		uiView.findComponent("link_website", MenuItem).onClick = link_website_onClick;
 		uiView.findComponent("link_github", MenuItem).onClick = link_github_onClick;
+		// set the label to reflect the game version defined in Project.xml
 		uiView.findComponent("lbl_version", Label).text = haxe.macro.Compiler.getDefine("GAME_VERSION");
-		// get a reference to our lists
-		inputLayerList = uiView.findComponent("lst_input_layer", ListView);
-		outputLayerList = uiView.findComponent("lst_output_layer", ListView);
+		// get a reference to our layer lists
+		layerLists = [for (i in 0...2) new ListView()];
+		layerLists[0] = uiView.findComponent("lst_input_layer", ListView); // input layer list
+		layerLists[1] = uiView.findComponent("lst_hidden_layer", ListView); // hidden layer list
+		layerLists[2] = uiView.findComponent("lst_output_layer", ListView); // output layer list
+		// get a reference to our weights lists
+		weightsLists = [for (i in 0...1) new ListView()];
+		weightsLists[0] = uiView.findComponent("lst_inpToHid_weights", ListView); // input to hidden weights list
+		weightsLists[1] = uiView.findComponent("lst_hidToOut_weights", ListView); // hidden to output weights list
 	}
 
 	function btn_initPerceptron_onClick(_) {
-		perc = new Perceptron(5);
+		perc = new Perceptron(5, 3);
 
-		// clear the list of example values
-		inputLayerList.dataSource.clear();
+		// clear all lists of previous values
+		for (list in layerLists) {
+			list.dataSource.clear();
+		}
+		for (list in weightsLists) {
+			list.dataSource.clear();
+		}
 
+		// fill layer lists
 		for (neuron in perc.inputLayer) {
-			inputLayerList.dataSource.add(neuron);
+			layerLists[0].dataSource.add(neuron);
+		}
+
+		for (neuron in perc.hiddenLayer) {
+			layerLists[1].dataSource.add(neuron);
+		}
+
+		for (neuron in perc.outputLayer) {
+			layerLists[2].dataSource.add(neuron);
+		}
+		// fill weights list
+		var inputToHiddenConns = perc.inputLayer.length * perc.hiddenLayer.length;
+		for (i in 0...inputToHiddenConns) {
+			weightsLists[0].dataSource.add(perc.weights[i]);
+		}
+		var hiddenToInputConns = perc.hiddenLayer.length * perc.outputLayer.length;
+		for (i in inputToHiddenConns...inputToHiddenConns + hiddenToInputConns) {
+			weightsLists[1].dataSource.add(perc.weights[i]);
 		}
 	}
 
